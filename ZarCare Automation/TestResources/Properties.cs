3046525ed6adc0257ac81ae20780587cf502342a
sahil705 <sahil.raina@lngconsultancy.co.in>
2024-09-05 16:21:00 +0530

    Push new change

--- ZarCare Automation/TestResources/Properties.cs
+++ ZarCare Automation/TestResources/Properties.cs
@@ -5,6 +5,7 @@
         public static string environment = "Preprod";
         public static string browser = "Chrome";
         public static string QA = "Sahil Sharma";
-        public static string report = "C:\\Zarcare_Automation_Project\\Report\\";
+        public static string report = "C:\\Zarcare_New_Automation_Project\\Report\\";
+
     }
 }
