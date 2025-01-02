d45bbcb3537af660df823d5da77f204666d4dde1
sahil705 <sahil.raina@lngconsultancy.co.in>
2025-01-02 11:54:01 +0530

    Patient Dashboard test case id (57,58,59) completed

--- ZarCare Automation/Test/PageActions/Patient_Dashboard_Page.cs
+++ ZarCare Automation/Test/PageActions/Patient_Dashboard_Page.cs
@@ -6,6 +6,8 @@ namespace ZarCare_Automation.Test.PageActions
     {
         public static Patient_Dashboard_Page_Locator PatientDashboardPage = new Patient_Dashboard_Page_Locator();
         public static Active_Appointment_Locator ActiveAppointmentPage = new Active_Appointment_Locator();
+        public static CheckOut_Page_Locator CheckoutPage = new CheckOut_Page_Locator();
+
         public static void ValidatePatientDashboard()
         {
             Wait.WaitTillPageLoad();
@@ -199,9 +201,86 @@ namespace ZarCare_Automation.Test.PageActions
             }
         }
 
-       
-    }
+       public static void ValidateRepeatPrescriptionJourney(int doctorId, string appointmentNumber, string doctorPopupText, string appointmentPopupText,string voucherCode, string voucherSuccessMessage)
+       {
+            DateTime? dateCreated;
+            ScrollToBottoms();
 
+            IList<IWebElement> getAppointmentRecords = PatientDashboardPage.Web_AppointmentRecords;
+            bool isActionCompleted = false;
+            foreach (IWebElement appointment in getAppointmentRecords)
+            {
+                IWebElement appointmentReferenceNumber = appointment.FindElement(PatientDashboardPage.By_ReferenceNumber);
+                string refNumber = appointmentReferenceNumber.Text;
+
+
+                if (refNumber.Equals(appointmentNumber))
+                {
+                    IList<IWebElement> repeatRequestButton = appointment.FindElements(PatientDashboardPage.By_RepeatRequestButton);
+
+                    if (repeatRequestButton.Count > 0 && repeatRequestButton[0].Displayed)
+                    {
+                        repeatRequestButton[0].Click();
+                        bool isDoctorAvailable = Database_Utils.GetDoctorAndAppointmentDetail(doctorId, appointmentNumber, out dateCreated);
+
+                        if (!isDoctorAvailable)
+                        {
+                            Console.WriteLine("Provider is not available.");
+                            Wait.ElementIsVisible(PatientDashboardPage.By_DoctorNotAvailableHeader, 5);
+                            string popupText = PatientDashboardPage.Web_DoctorNotAvailablePopupText.Text;
+                            StringAssert.Contains(doctorPopupText, popupText);
+
+                            Reports.childLog.Log(Status.Info, "Provider no longer available popup is displayed ");
+                            Generic_Utils.GetScreenshot("Provider no longer available Screenshot");
+
+                        }
+                        else if (dateCreated.HasValue)
+                        {
+                            DateTime appointmentDate = dateCreated.Value.Date;
+                            if ((DateTime.Now - dateCreated.Value).TotalDays > 90)
+                            {
+                                Console.WriteLine("Appointment is older than 3 months.");
+                                Wait.ElementIsVisible(PatientDashboardPage.By_DoctorNotAvailableHeader, 5);
+                                string getPopupText = PatientDashboardPage.Web_PastThreeMonthAppointmentPopupText.Text;
+                                Assert.That(getPopupText,Is.EqualTo(appointmentPopupText));
+
+                                Reports.childLog.Log(Status.Info, "Past Three Month Appointment popup is displayed ");
+                                Generic_Utils.GetScreenshot("Past Three Month Appointment Screenshot");
+                            }
+                            else
+                            {
+                                Wait.GenericWait(2000);
+                                Generic_Utils.IsElementDisplayed(PatientDashboardPage.By_CheckOutPageElement);
+
+                                CheckoutPage.Web_Voucher_TextBox.SendKeys(voucherCode);
+                                CheckoutPage.Web_Voucher_Consent_Checkbox.Click();
+                                CheckoutPage.Web_Voucher_Apply_Button.Click();
+                                Wait.ElementIsVisible(CheckoutPage.By_Voucher_Success_Message_Text, 5);
+
+                                string voucherSuccessText = CheckoutPage.Web_Voucher_Success_Message_Text.Text;
+                                Assert.That(voucherSuccessText, Is.EqualTo(voucherSuccessMessage));
+
+                                ScrollToBottoms();
+                                PatientDashboardPage.Web_Repeat_Prescription_Submit_Button.Click();
+                                
+                                Reports.childLog.Log(Status.Info, "Repeat Prescription Journey is Completed ");
+                                Generic_Utils.GetScreenshot("Repeat Prescription Journey Confirmation Page Screenshot");
+
+                            }
+                        }
+                        isActionCompleted = true; 
+                        break;
+
+                    }
+                    else
+                    {
+                        // Handle the case where the button is not found
+                        Console.WriteLine("Repeat request button is not found for the respective appointment.");
+                    }
+                }
+            }
+        }
+    }
 
 }
 
